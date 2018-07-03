3b69e293230a6027f6c567ebc0a1da6a0b03846e
william <williamsakata@hotmail.com>
2018-07-03 09:29:59 -0300

    Começando inserção

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 #define maxkey 4
 #define minkey maxkey/2
 #define nokey '@'
 #define erro -10
+#define true 1
+#define false 0
+#define promocao = 10
+#define sem_promo = 5
 
 typedef struct {
     short keycout;
@@ -22,13 +25,10 @@ int main(){
     arvore = fopen("arvore.txt", "w+");
 }
 
-bool busca(int rrn, int key, int found_rrn, int found_pos){
-    FILE *chaves;
+int busca(int rrn, int key, int found_rrn, int found_pos, FILE *chaves){
     pagina *pag;
-    int pos=0 ;
-    bool found = false;
+    int pos=0, found = false ;
 
-    chaves = fopen("chaves.txt", "r");
 
     if(rrn = NULL)
         return 0;
@@ -48,12 +48,37 @@ bool busca(int rrn, int key, int found_rrn, int found_pos){
             found_pos = pos;
             return true;
         } else{
-            return busca(pag->child[pos], key, found_rrn, found_pos);
+            return busca(pag->child[pos], key, found_rrn, found_pos, chaves);
         }
     }
 }
 
-int insere(int rrn_atual, int key, )
+int insere(int rrn_atual, int key, int filho_d_pro, int chave_pro, FILE *chaves){ //da pra retornar o ponteiro pro arquivo?
+    pagina *pag;
+    int pos=0;
+
+    if(rrn_atual == NULL){
+        chave_pro = key;
+        filho_d_pro = NULL;
+        return promocao;
+    } else{
+        fseek(chaves, rrn_atual+4, SEEK_SET);
+        fread(pag, sizeof(pagina), 1, chaves);
+
+        while(key != pag->key && pag->key <= 4){
+            pos++;
+        }
+
+        if(pos < 4){
+            printf("\nError!!! Chave Duplicada!!");
+            return erro;
+        }
+
+        return insere(pag->child[pos], key, ) //como que retorna o rrn_pro e o chv_pro
+    }
+
+
+}
 
 
 
