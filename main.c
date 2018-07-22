a491b49ccab47cad800d630c56ac981ae6748d6d
william <williamsakata@hotmail.com>
2018-07-22 20:29:32 -0300

    Inserção com erros

--- main.c
+++ main.c
@@ -1,26 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define maxchave 4
-#define minchave maxkey/2
-#define nokey '@'
-#define erro -10
-#define true 1
-#define false 0
-#define promocao = 10
-#define sem_promo = 5
+//************* ALTERACAO *******************
+// Alterei todos os nomes usados nos defines para caixa alta
+// Com caixa baixa estava dando problema no true/false
+#define MAXCHAVE 4
+#define MINCHAVE MAXCHAVE/2
+#define NOKEY '@'
+#define ERRO -10
+#define TRUE 1
+#define FALSE 0
+//************* ALTERACAO *******************
+//#define promocao = 10
+//#define sem_promo = 5
+#define PROMOCAO 10
+#define SEMPROMO 5
+// Esse NIL não é a mesma coisa que NULL
+#define NIL -1
+//*******************************************
 
 typedef struct {
-    short keycout;
-    char key[maxchave];
-    short child[maxchave+1];
+    short keycount;
+    char key[MAXCHAVE];
+    short child[MAXCHAVE+1];
 } pagina;
 
 typedef struct {
     short keycount;
-    char key[maxchave+1];
-    short child[maxchave+2];
+    char key[MAXCHAVE+1];
+    short child[MAXCHAVE+2];
 } pagaux;
+
+//************* ALTERACAO *******************
+// Inclui os protótipos das funções
+int busca(int rrn, int key, int found_rrn, int found_pos, FILE *chaves);
+int insere(int rrn_atual, int key, int *filho_d_pro, int *chave_pro, FILE *chaves);
+void divide(int chave_i, int rrn_i, pagina *pag, int *chave_pro, int *filho_d_pro, pagina *novapag, FILE *chaves);
+//*******************************************
 
 
 int main(){
@@ -33,60 +48,72 @@ int main(){
 
 int busca(int rrn, int key, int found_rrn, int found_pos, FILE *chaves){
     pagina *pag;
-    int pos=0, found = false ;
+    int pos=0, found = FALSE ;
 
-
-    if(rrn = NULL)
+    if(rrn = NIL)
         return 0;
-
+        //*******************************************
     else{
         fseek(chaves, (rrn* sizeof(pagina)+4), SEEK_SET);
         fread(pag, sizeof(pagina), 1, chaves);
 
-        while(found == false && pag->key <= 4){
-            if(pag->key[pos]==key)
-                found = true;
+        //************* ALTERACAO *******************
+        // Resolvi o erro de compilação, mas esse looping não vai funcionar direito
+        //while(!found && pag->key <= 4){
+        while(!found && pag->keycount <= 4){
+            if(pag->key[pos] == key)
+                found = TRUE;
             pos++;
         }
+        //*******************************************
 
         if(found){
             found_rrn = rrn;
             found_pos = pos;
-            return true;
+            return TRUE;
         } else{
             return busca(pag->child[pos], key, found_rrn, found_pos, chaves);
         }
     }
 }
 
-int insere(int rrn_atual, int key, int filho_d_pro, int chave_pro, FILE *chaves){ //da pra retornar o ponteiro pro arquivo?
+//************* ALTERACAO *******************
+// filho_d_pro e chave_pro têm que ser ponteiros, pois eles são parâmetros de retorno
+//int insere(int rrn_atual, int key, int filho_d_pro, int chave_pro, FILE *chaves)
+int insere(int rrn_atual, int key, int *filho_d_pro, int *chave_pro, FILE *chaves){ //da pra retornar o ponteiro pro arquivo?
+    // Se vc declarar pag e novapag como ponteiros, vai ter que usar malloc para alocar espaço para elas
+    // O mais fácil seria declarar como var normal e passar o endereço quando precisar ser ponteiro
     pagina *pag;
     pagina *novapag;
-    int pos=0, retorno, fit, i=0 ,j, rrn_pro = filho_d_pro, chv_pro = chave_pro;
+    //Arrumei o erro de compilação, mas essa inicialização do rrn_pro e do chv_pro não vai dar certo
+    //int pos=0, retorno, fit, i=0 ,j, rrn_pro = filho_d_pro, chv_pro = chave_pro;
+    int pos=0, retorno, fit, i=0 ,j, rrn_pro = *filho_d_pro, chv_pro = *chave_pro;
 
-    if(rrn_atual == NULL){
-        chave_pro = key;
-        filho_d_pro = NULL;
-        retorno = promocao;
+    if(rrn_atual == NIL){
+        *chave_pro = key;
+        //filho_d_pro = NULL;
+        *filho_d_pro = NIL;
+        retorno = PROMOCAO;
+
     } else{
         fseek(chaves, rrn_atual+4, SEEK_SET);
         fread(pag, sizeof(pagina), 1, chaves);
 
-        while(key != pag->key && pag->key <= 4){
+        while(key != pag->key[pos] && pag->keycount <= 4){
             pos++;
         }
 
         if(pos < 4){
             printf("\nError!!! Chave Duplicada!!");
-            retorno = erro;
+            retorno = ERRO;
         }
 
-        retorno = insere(pag->child[pos], key, rrn_pro, chv_pro, chaves); //como que retorna o rrn_pro e o chv_pro
+        retorno = insere(pag->child[pos], key, &rrn_pro, &chv_pro, chaves); //como que retorna o rrn_pro e o chv_pro
     }
-    if(retorno == erro || retorno == sem_promo){
+    if(retorno == ERRO || retorno == SEMPROMO){
         return retorno;
     } else{
-        if(pag->keycout < maxchave){
+        if(pag->keycount < MAXCHAVE){
 
             while(pag->key[i] < chv_pro){
                 i++;
@@ -107,25 +134,28 @@ int insere(int rrn_atual, int key, int filho_d_pro, int chave_pro, FILE *chaves)
 
             fwrite(pag, sizeof(pag), 1, chaves);
 
-            return sem_promo;
+            return SEMPROMO;
         } else{
-            divide(chv_pro, rrn_pro, pag, chave_pro, filho_d_pro, novapag, chaves); //perguntar sobre a novapag, onde cria
+
+            divide(chv_pro, rrn_pro, pag, chave_pro, filho_d_pro, novapag, chaves);
 
             fseek(chaves, rrn_atual, SEEK_SET+4);
             fwrite(pag, sizeof(pag), 1, chaves);
 
-            fseek(chaves, filho_d_pro, SEEK_SET+4);
+            fseek(chaves, *filho_d_pro, SEEK_SET+4);
             fwrite(novapag, sizeof(novapag), 1, chaves);
         }
     }
 }
 
-void divide(int chave_i, int rrn_i, pagina *pag, int chave_pro, int filho_d_pro, pagina *novapag, FILE *chaves){
+void divide(int chave_i, int rrn_i, pagina *pag, int *chave_pro, int *filho_d_pro, pagina *novapag, FILE *chaves){
     pagaux *pagaux1;
     pagina *novapag1;
     int i=0, j=0, rrn_nova;
 
-    pagaux1 = pag;
+    //pagaux e pagina são tipos diferentes, portanto vc não pode fazer essa atribuicao
+    //pagaux1 = pag;
+    //para copiar os elementos de pag para pagaux1, tem que passar um por um
 
     while(pagaux1->key[i] < chave_i){
         i++;
@@ -141,12 +171,12 @@ void divide(int chave_i, int rrn_i, pagina *pag, int chave_pro, int filho_d_pro,
     }
     pagaux1->child[i+1] = rrn_i;
 
-    chave_pro = pagaux1->key[2];
+
+    *chave_pro = pagaux1->key[2];
 
     fseek(chaves, 0, SEEK_END);
 
-
-    filho_d_pro = ((ftell(chaves)/ sizeof(pagina)) - 4);
+    *filho_d_pro = ((ftell(chaves)/ sizeof(pagina)) - 4);
 
     for(j=0; j<i;j++){
         pag->key[j] = pagaux1->key[j];
@@ -157,45 +187,3 @@ void divide(int chave_i, int rrn_i, pagina *pag, int chave_pro, int filho_d_pro,
     }
 
 }
-
-
-
-
-
-
-
-
-
-/*
-struct No{
-    int numero;
-    struct No *esquerda;
-    struct No *direita;
-};
-typedef struct No No;
-
-void criarArvore(No **pRaiz);
-void insercao(No **Raiz, int numero);
-
-int main() {
-
-}
-
-void criarArvore(No **pRaiz){
-    *pRaiz = NULL;
-}
-
-void insercao(No **Raiz, int numero){
-    if(*Raiz == NULL){
-        *Raiz=(No *)malloc(sizeof(No));
-        (*Raiz)->direita = NULL;
-        (*Raiz)->esquerda= NULL;
-        (*Raiz)->numero = numero;
-    } else{
-        if (numero<((*Raiz)->numero))
-            insercao(&((*Raiz)->esquerda), numero);
-        else
-            insercao(&((*Raiz)->direita), numero);
-    }
-}
- */
